63cd84853f305a94c1846c2957185980ec54e352
Raúl <raul2040@outlook.es>
2018-05-06 17:56:52 +0200

    Intentado leer los dos json

--- JS/functions.js
+++ JS/functions.js
@@ -12,4 +12,17 @@ $(document).ready(function() {
             $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
         });
     }
+
+    $.getJSON("../DATA/2.json", function (jsonObject) {
+        //en aquest punt l'objecte jsonObject correspon al fitxer
+        console.log("HA ENTRADO EN EL SEGUNDO JSON")
+        pintar1(jsonObject);
+    });
+
+    function pintar1(json) {
+        $.each(json, function (i, news) {
+            $("#news1").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+        });
+    }
+
 })
