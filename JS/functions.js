752082d230a403787416c797a984074d7758eed1
Raúl <raul2040@outlook.es>
2018-05-06 17:17:56 +0200

    Añadiendo las noticias

--- JS/functions.js
+++ JS/functions.js
@@ -7,19 +7,7 @@ $(function () {
 
     function pintar(json) {
         $.each(json, function (i, news) {
-            $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+            $("#news1").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
         });
     }
-    
-    $.getJSON("../DATA/2.json", function (jsonObject) {
-        //en aquest punt l'objecte jsonObject correspon al fitxer
-        pintar(jsonObject);
-    });
-
-    function pintar(json) {
-        $.each(json, function (i, news) {
-            $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
-        });
-    }
-
 })
