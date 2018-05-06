6689a95a278ccca1ecafd80eeb07f999c1606426
Raúl <raul2040@outlook.es>
2018-05-06 17:14:50 +0200

    Añadiendo las noticias

--- JS/functions.js
+++ JS/functions.js
@@ -1,14 +1,25 @@
-$(function(){
+$(function () {
 
-$.getJSON( "../DATA/1.json", function( jsonObject ) {
-    //en aquest punt l'objecte jsonObject correspon al fitxer
-    pintar( jsonObject );
-  });
-  
-  function pintar(json){
-   $.each( json, function( i, news ) {
-    $("#news").append( "<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>"+ "</div>" );
-   });
-  }
+    $.getJSON("../DATA/1.json", function (jsonObject) {
+        //en aquest punt l'objecte jsonObject correspon al fitxer
+        pintar(jsonObject);
+    });
+
+    function pintar(json) {
+        $.each(json, function (i, news) {
+            $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+        });
+    }
+    
+    $.getJSON("../DATA/2.json", function (jsonObject) {
+        //en aquest punt l'objecte jsonObject correspon al fitxer
+        pintar(jsonObject);
+    });
+
+    function pintar(json) {
+        $.each(json, function (i, news) {
+            $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+        });
+    }
 
 })
