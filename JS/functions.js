09598cb5d33709b0e13a386e80dc17b9ba02ace2
Raúl <raul2040@outlook.es>
2018-05-06 16:04:19 +0200

    Testing read json

---
+++ JS/functions.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+
+$.getJSON( "news1.json", function( jsonObject ) {
+    //en aquest punt l'objecte jsonObject correspon al fitxer
+    pintar( jsonObject );
+  });
+  
+  function pintar(json){
+   $.each( json, function( i, news ) {
+    $("#news").append( "<li id='" + i + "'>" + news.title + " " + news.description + "</li>" );
+   });
+  }
+
+});
