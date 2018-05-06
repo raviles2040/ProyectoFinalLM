96b74b930b31788f43cf77819fec1f3b45b188bf
Raúl <raul2040@outlook.es>
2018-05-06 17:24:04 +0200

    Añadiendo las 6 noticias

--- JS/functions.js
+++ JS/functions.js
@@ -9,5 +9,12 @@ $(function () {
         $.each(json, function (i, news) {
             $("#news1").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
         });
+
+        if(json != $.getJSON("../DATA/2.json")) {
+            $.getJSON("../DATA/2.json", function (jsonObject) {
+                //en aquest punt l'objecte jsonObject correspon al fitxer
+                pintar(jsonObject);
+            });
+        }
     }
 })
--- JS/functions1.js
+++ JS/functions1.js
@@ -1,12 +1,12 @@
 $(function () {
-$.getJSON("../DATA/2.json", function (jsonObject) {
-    //en aquest punt l'objecte jsonObject correspon al fitxer
-    pintar(jsonObject);
-});
-
-function pintar(json) {
-    $.each(json, function (i, news) {
-        $("#news2").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+    $.getJSON("../DATA/2.json", function (jsonObject) {
+        //en aquest punt l'objecte jsonObject correspon al fitxer
+        pintar(jsonObject);
     });
-}
+
+    function pintar(json) {
+        $.each(json, function (i, news) {
+            $("#news2").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+        });
+    }
 })
