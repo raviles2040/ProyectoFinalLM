32b41293e04d8d38d8849ebb97487f898280ec10
Raúl <raul2040@outlook.es>
2018-05-10 22:51:39 +0200

    Modificando el javascript para que cargue las noticias hasta dos veces

--- JS/functions.js
+++ JS/functions.js
@@ -1,30 +1,51 @@
 $(document).ready(function () {
     $("button").click(function () {
         var contador = 0;
-        console.log("ha entrado en el documento JS")
         $.getJSON("../DATA/1.json", function (jsonObject) {
-            //en aquest punt l'objecte jsonObject correspon al fitxer
             console.log(jsonObject)
-            pintar(jsonObject);
+            volcarJson(jsonObject);
         });
 
-        function pintar(json) {
-            console.log("JSON --> ", json)
-            $.each(json, function (i, news) {
-                $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
-            });
+        function volcarJson(json) {
+            switch (contador) {
+                case 0:
+                    imprimirTresPrimerasNoticias(json)
+                    break;
+                case 1:
+                    imprimirTresUltimasNoticias(json)
+                    break;
+                default:
+                    return alert("No hay más noticias que cargar")
+                    break;
+            }
         }
 
-        $.getJSON("../DATA/2.json", function (jsonObject) {
-            //en aquest punt l'objecte jsonObject correspon al fitxer
-            console.log("HA ENTRADO EN EL SEGUNDO JSON")
-            pintar1(jsonObject);
-        });
+        function imprimirTresPrimerasNoticias(json) {
+            if (contador == 0) {
+                console.log("JSON --> ", json)
+                $.each(json, function (i, news) {
+                    $("#news").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+                });
+                contador++;
+            }
+            else {
 
-        function pintar1(json) {
-            $.each(json, function (i, news) {
-                $("#news1").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+            }
+        }
+
+        function imprimirTresUltimasNoticias(json) {
+            $.getJSON("../DATA/2.json", function (jsonObject) {
+                //en aquest punt l'objecte jsonObject correspon al fitxer
+                console.log("HA ENTRADO EN EL SEGUNDO JSON")
+                pintar1(jsonObject);
             });
+
+            function pintar1(json) {
+                $.each(json, function (i, news) {
+                    $("#news1").append("<div'" + i + "'>" + "<h1>" + news.title + "</h1>" + "<br>" + "<img src=" + news.img + ">" + "<p>" + news.description + "</p>" + "</div>");
+                });
+                contador++;
+            }
         }
     });
 });
