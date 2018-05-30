70970c9fbea5fa21ebb38cf704b82a49ddaa65fa
Raúl <raul2040@outlook.es>
2018-05-30 20:25:47 +0200

    Implementando el scroll botom

--- JS/functions.js
+++ JS/functions.js
@@ -1,69 +1,61 @@
 let contador = 0;
 
+function pintarJson();
+switch (contador) {
+    case 0:
+        imprimirTresPrimerasNoticias()
+        break;
+    case 1:
+        imprimirTresUltimasNoticias()
+        break;
+    default:
+        break;
+}
+function imprimirTresPrimerasNoticias() {
+    $.getJSON("../DATA/1.json", function (jsonObject) {
+        $.each(jsonObject, function (i, news) {
+            if (i < 2) {
+                $("#news").append("<div class=\"col-sm-6\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
+                    "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
+                    "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
+                    "</div>");
+            }
+            else {
+                $("#news").append("<div class=\"col-sm-12\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
+                    "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
+                    "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
+                    "</div>");
+            }
+        });
+        contador++;
+    });
+}
+
+function imprimirTresUltimasNoticias() {
+    $.getJSON("../DATA/2.json", function (jsonObject) {
+        $.each(jsonObject, function (i, news) {
+            if (i < 2) {
+                $("#news").append("<div class=\"col-sm-6\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
+                    "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
+                    "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
+                    "</div>");
+            }
+            else {
+                $("#news").append("<div class=\"col-sm-12\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
+                    "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
+                    "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
+                    "</div>");
+            }
+        });
+        contador++;
+    });
+}
+
 $(document).ready(function () {
     $("#leerMas").click(function () {
-        $.getJSON("../DATA/1.json", function (jsonObject) {
-            volcarJson(jsonObject);
-        });
-
-        function volcarJson(json) {
-            switch (contador) {
-                case 0:
-                    imprimirTresPrimerasNoticias(json)
-                    break;
-                case 1:
-                    imprimirTresUltimasNoticias(json)
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        function imprimirTresPrimerasNoticias(json) {
-            $.each(json, function (i, news) {
-                if (i < 2) {
-                    $("#news").append("<div class=\"col-sm-6\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
-                        "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
-                        "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
-                        "</div>");
-                }
-                else {
-                    $("#news").append("<div class=\"col-sm-12\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
-                        "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
-                        "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
-                        "</div>");
-                }
-            });
-            contador++;
-        }
-
-        function imprimirTresUltimasNoticias(json) {
-            $.getJSON("../DATA/2.json", function (jsonObject) {
-                //en aquest punt l'objecte jsonObject correspon al fitxer
-                console.log("HA ENTRADO EN EL SEGUNDO JSON")
-                pintar1(jsonObject);
-            });
-
-            function pintar1(json) {
-                $.each(json, function (i, news) {
-                    if (i < 2) {
-                        $("#news").append("<div class=\"col-sm-6\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
-                            "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
-                            "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\"data-indice=" + news.indice + ">Leer más</button>" +
-                            "</div>");
-                    }
-                    else {
-                        $("#news").append("<div class=\"col-sm-12\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
-                            "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +
-                            "<button type=button class=\"btn btn-info btn-lg \" + id=\"nuevaNoticia\" data-indice=" + news.indice + ">Leer más</button>" +
-                            "</div>");
-                    }
-                });
-                $("#leerMas").css("display", "none")
-                contador++;
-            }
-        }
+        pintarJson();
     });
+
     $("#noticiaCompleta1").click(function () {
         window.location.href = "../NEWS/news1.html"
     });
@@ -74,35 +66,29 @@ $(document).ready(function () {
         window.location.href = "../NEWS/news3.html"
     });
     $("#nuevaNoticia").click(function () {
-        indiceNoticia = $("#nuevaNoticia").data("indice")
+        let indiceNoticia = $("#nuevaNoticia").data("indice")
         rellenarTemplate(indiceNoticia);
     });
 
-    function rellenarTemplate(indice) {
-        if (indice <= 3) {
-            $.getJSON("../DATA/1.json", function (jsonObject) {
-                //en aquest punt l'objecte jsonObject correspon al fitxer
-                console.log("HA ENTRADO EN EL SEGUNDO JSON")
-                pintarNoticia(jsonObject, indice);
-            });
-        }
-        else {
-            $.getJSON("../DATA/2.json", function (jsonObject) {
-                //en aquest punt l'objecte jsonObject correspon al fitxer
-                console.log("HA ENTRADO EN EL SEGUNDO JSON")
-                pintarNoticia(jsonObject, indice);
-            });
-        }
-    }
     function pintarNoticia(jsonObject, indice) {
         $.each(json, function (i, news) {
             if (news.indice == indice) {
                 $("#introduceNuevaNoticia").append("<div class=\"col-sm-12\" style=\"background-color:lavenderblush;\" + " + i + ">" + "<h1>" + news.title +
-                    "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" +"</div>");
+                    "</h1>" + "<br>" + "<img src=" + news.img + " class=\"col-sm-12\" style=\"background-color:lavenderblush;\" " + ">" + "<p>" + news.description + "</p>" + "</div>");
             }
         });
         window.location.href = "../NEWS/newNews.html"
     }
+
+    $(window).on("scroll", function () {
+        var scrollHeight = $(document).height();
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+        if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+            pintarJson();
+        }
+    });
 });
+
+
 
 
